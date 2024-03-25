9471799fbef2c53a05da02c472d839b86b767967
DESKTOP-VHRR1O7\hongje <ajyong316@gmail.com>
2024-03-25 22:45:01 +0900

    JPA, spring data jpa set up

--- src/main/java/hello/itemservice/config/JpaConfig.java
+++ src/main/java/hello/itemservice/config/JpaConfig.java
@@ -1,19 +1,22 @@
 package hello.itemservice.config;
 
 import hello.itemservice.repository.ItemRepository;
-import hello.itemservice.repository.mybatis.ItemMapper;
-import hello.itemservice.repository.mybatis.MyBatisItemRepository;
+import hello.itemservice.repository.jpa.JpaItemRepository;
 import hello.itemservice.service.ItemService;
 import hello.itemservice.service.ItemServiceV1;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@RequiredArgsConstructor
-public class MyBatisConfig {
+import javax.persistence.EntityManager;
 
-    private final ItemMapper itemMapper;
+@Configuration
+public class JpaConfig {
+
+    private final EntityManager em;
+
+    public JpaConfig(EntityManager em) {
+        this.em = em;
+    }
 
     @Bean
     public ItemService itemService() {
@@ -22,6 +25,6 @@ public class MyBatisConfig {
 
     @Bean
     public ItemRepository itemRepository() {
-        return new MyBatisItemRepository(itemMapper);
+        return new JpaItemRepository(em);
     }
 }
--- src/main/java/hello/itemservice/domain/Item.java
+++ src/main/java/hello/itemservice/domain/Item.java
@@ -2,17 +2,23 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
+@Table(name = "item")
 public class Item {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "item_name", length = 10)
     private String itemName;
     private Integer price;
     private Integer quantity;
 
-    public Item() {
-    }
+    // JPA 는 public, protected 기본 생성자를 필수로 넣어주어야 한다.
+    public Item() {}
 
     public Item(String itemName, Integer price, Integer quantity) {
         this.itemName = itemName;
